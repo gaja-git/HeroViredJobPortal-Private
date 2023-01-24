07c44222abba46d9dc95dcac38b449196becb430
Vidhya Mani <vidhyamaniece@gmail.com>
2023-01-24 23:22:52 +0530

    Searchbox section completed

--- jobportal-react-app/src/components/Dashboard/Searchbox/Searchbox.js
+++ jobportal-react-app/src/components/Dashboard/Searchbox/Searchbox.js
@@ -1,12 +1,57 @@
 import React from "react"
+import './searchbox.css'
 
-export default function Searchbox(){
-	return(
-        <div>
-            <div className="searchbox">
-                  <input type="text" placeholder='Search'></input>
-                  <i className="fa fa-search"></i>
-               </div>
-        </div>
-	)
+export default function Searchbox() {
+        return (
+                <div className="searchbox">
+                        <div className="search_v">
+                                <input type="text" placeholder='Title,Client,Location...'></input>
+                                <button>Search</button>
+                        </div>
+                        <div className="drop_v">
+                                <select name="all_v" id="all_v">
+                                        <option value="All Jobs">All Jobs</option>
+                                        <option value="Associate">Associate</option>
+                                        <option value="Software Engineer">Software Engineer</option>
+                                        <option value="Team Lead">Team Lead</option>
+                                        <option value="HR">HR</option>
+                                        <option value="Product Manager">Product Manager</option>
+
+                                </select>
+                                <select name="ind_v" id="ind_v">
+                                        <option value="Industry">Industry</option>
+                                        <option value="IT">IT</option>
+                                        <option value="Fashion">Fashion</option>
+                                        <option value="Education">Education</option>
+                                        <option value="Design">Design</option>
+                                        <option value="Research">Research</option>
+                                </select>
+                                <select name="loc_v" id="loc_v">
+                                        <option value="Location">Location</option>
+                                        <option value="Bangalore">Bangalore</option>
+                                        <option value="Chennai">Chennai</option>
+                                        <option value="Hyderabad">Hyderabad</option>
+                                        <option value="Gurugram">Gurugram</option>
+                                        <option value="Mumbai">Mumbai</option>
+                                </select>
+                                <select name="jtype_v" id="jtype_v">
+                                        <option value="Job Type">Job Type</option>
+                                        <option value="Full-time">Full-time</option>
+                                        <option value="Part-time">Part-time</option>
+                                        <option value="Freelancer">Freelancer</option>
+                                        <option value="Internship">Internship</option>
+
+                                </select>
+                                <select name="date_v" id="date_v">
+                                        <option value="Posted Date">Posted Date</option>
+                                        <option value="January -March">January -March</option>
+                                        <option value="April-June">April-June</option>
+                                        <option value="July-September">July-September</option>
+                                        <option value="October-December">October-December</option>
+                                </select>
+                                    
+                        </div>
+                </div>
+
+        )
 }
