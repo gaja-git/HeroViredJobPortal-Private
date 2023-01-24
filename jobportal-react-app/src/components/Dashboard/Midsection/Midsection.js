8911fed0a517e9731dc4ca588aa488e239d9fffa
Gaja Jothi <gajalakshmi.a@gmail.com>
2023-01-24 14:16:11 +0530

    completed job content and midsection

---
+++ jobportal-react-app/src/components/Dashboard/Midsection/Midsection.js
@@ -0,0 +1,139 @@
+import React from "react"
+import './midsection.css'
+
+export default function Midsection() {
+    return (
+        <div className="midsection_gj">
+
+            <div className="snapshot_outside_boundary_gj">
+                <h5 className="snapshot_gj">Snapshots</h5>
+                <div className="snapshot_inner_item_boundary_gj">
+
+                    <div className="all_job_section_gj">
+                        <div className="all_job_inner_frame_gj">
+                            <div className="all_job_icon_frame_gj">
+                                <i className="fa fa-user-circle" ></i>
+                            </div>
+                            <div className="all_job_text_display_gj">
+                                <div className="all_job_text1_gj">
+                                    All Jobs
+                                </div >
+                                <div className="all_job_text2_gj">
+                                    100
+                                </div>
+                            </div>
+                            {/* <div className="all_jobs_right_arrow">
+
+                            </div> */}
+
+                        </div>
+                    </div>
+                    <div className="all_job_section_gj">
+                        <div className="all_job_inner_frame_gj">
+                            <div className="all_job_icon_frame_gj">
+                                <i class="fa fa-user-circle" ></i>
+                            </div>
+                            <div className="all_job_text_display_gj">
+                                <div className="all_job_text1_gj">
+                                    Eligible Jobs
+                                </div >
+                                <div className="all_job_text2_gj">
+                                    100
+                                </div>
+                            </div>
+                            {/* <div className="all_jobs_right_arrow">
+                            </div> */}
+
+                        </div>
+                    </div>
+                    <div className="all_job_section_gj">
+                        <div className="all_job_inner_frame_gj">
+                            <div className="all_job_icon_frame_gj">
+                                <i class="fa fa-user-circle" ></i>
+                            </div>
+                            <div className="all_job_text_display_gj">
+                                <div className="all_job_text1_gj">
+                                    Applied Jobs
+                                </div >
+                                <div className="all_job_text2_gj">
+                                    50
+                                </div>
+                            </div>
+                            {/* <div className="all_jobs_right_arrow">
+                            </div> */}
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="myapp_outside_boundary_gj">
+                <h5 className="myapp_gj"> My Application</h5>
+                <div className="myapp_inner_item_boundary_gj">
+                    <div className="all_job_section_gj">
+                        <div className="all_job_inner_frame_gj">
+                            <div className="all_job_icon_frame_gj">
+                                <i class="fa fa-user-circle" ></i>
+                            </div>
+                            <div className="all_job_text_display_gj">
+                                <div className="all_job_text1_gj">
+                                    Shortlisted
+                                </div >
+                                <div className="all_job_text2_gj">
+                                    100
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="all_job_section_gj">
+                        <div className="all_job_inner_frame_gj">
+                            <div className="all_job_icon_frame_gj">
+                                <i class="fa fa-user-circle" ></i>
+                            </div>
+                            <div className="all_job_text_display_gj">
+                                <div className="all_job_text1_gj">
+                                    Interviewing
+                                </div >
+                                <div className="all_job_text2_gj">
+                                    100
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="all_job_section_gj">
+                        <div className="all_job_inner_frame_gj">
+                            <div className="all_job_icon_frame_gj">
+                                <i class="fa fa-user-circle" ></i>
+                            </div>
+                            <div className="all_job_text_display_gj">
+                                <div className="all_job_text1_gj">
+                                    Rejected
+                                </div >
+                                <div className="all_job_text2_gj">
+                                    35
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="all_job_section_gj">
+                        <div className="all_job_inner_frame_gj">
+                            <div className="all_job_icon_frame_gj">
+                                <i class="fa fa-user-circle" ></i>
+                            </div>
+                            <div className="all_job_text_display_gj">
+                                <div className="all_job_text1_gj">
+                                    Offer Recieved
+                                </div >
+                                <div className="all_job_text2_gj">
+                                    30
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <div></div>
+        </div >
+
+    )
+}
