8fe2609966e0c4af7078a6d9aba21f82fb6f10ba
Vidhya Mani <vidhyamaniece@gmail.com>
2023-01-26 23:07:05 +0530

    controller changes

--- jobportal-rails-app/app/controllers/login_controller.rb
+++ jobportal-rails-app/app/controllers/login_controller.rb
@@ -3,26 +3,27 @@ class LoginController < ApplicationController
     skip_before_action :verify_authenticity_token
 
     def index 
-        render html: "Hello from Login Page"
+        current_user = Useraccount.find_by_id(session[:current_user_id])
+        render json: current_user
     end
     def create 
         uval = Useraccount.find_by('email': params[:email])
         if uval.nil? 
             render json: "User account does not exit"
         else 
-            if (uval.authenticate(params[:password]))
-                session[:current_user_id] = uval.id
+            # if (uval.authenticate(params[:password]))
+                # session[:current_user_id] = uval.id
 
-                render html: "Login successfull!!"
-            else
-                render json: "Wrong password!"
-            end
+                render json: "Login successfull!!"
+            # else
+            #     render json: "Wrong password!"
+            # end
             # render json: "User account exists" 
         end
 
     end
     def destroy 
-        # session.delete(:current_user_id)
+        session.delete(:current_user_id)
         render json: "Logged out successfully! Bye!"
     end
 end
