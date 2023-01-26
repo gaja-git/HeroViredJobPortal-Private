048d374763a778051f14e2bb786e820e2e243c44
Vidhya Mani <vidhyamaniece@gmail.com>
2023-01-26 23:09:12 +0530

    minor changes

--- jobportal-react-app/src/components/Welcome/Login.js
+++ jobportal-react-app/src/components/Welcome/Login.js
@@ -6,30 +6,29 @@ import './login.css'
 import axios from 'axios';
 
 const Login = () => {
-    let [info, setInfo] = useState([]);
+    let [info, setInfo] = useState({});
     const navigate = useNavigate();
     
-    const handleLogin = (e) => {
+    function handleLogin(e){
         setInfo({
             ...info,
             [e.target.name]: e.target.value
-        })
+        });
     }
     console.log("info", info);
 
-    async function sendLogin() {
+    async function sendLogin(){
 
         console.log(info);
-        let res = await axios.post("/login", info)
-
+        let res = await axios.get('/login', info)
         console.log(res, "res");
-        if (res.data)
-            navigate("/profile");
+        // if (res.data)
+            navigate('/jobcanvas');
 
     }
     async function create_v() {
-        // let res = await axios.post("http://127.0.0.1:3001/bloguser", info)
-        // console.log(res.info);
+        let res = await axios.post("/useraccount", info)
+        console.log(res.info);
         navigate("/createaccount");
 
     }
@@ -61,28 +60,7 @@ const Login = () => {
                 {/* <input type="submit" Create Account onClick={create_v}  /> */}
                 <button onClick={create_v}>Please Create Account </button>
 
-            <div className="login_details">
-                <form >
-                    <br></br>
-                    <br></br>
-                    <label for="email"> E-mail ID: </label>
-                    <input type="text" id="email" placeholder='email' name='email' onChange={handleLogin} /><br></br>
-                    <label for="pass"> Password: </label>
-                    <input type="text" id="pass" placeholder='password' name='password' onChange={handleLogin} /><br></br>
-                    <input type="submit" className="login_submit" onClick={sendLogin} />
-                </form>
-
-
-                <span className="login_span"> Are you a new job seeker? </span><br></br>
-                {/* <span> Please create your account </span><br></br> */}
-                {/* <input type="submit" Create Account onClick={create_v}  /> */}
-                <button onClick={create_v}>Please Create Account </button>
-
-                {/* <Link to="/createaccount">
-                    <button>Create account!</button>
-                    </Link> */}
-                    {/* </div> */}
-                    </div>
+            
              </div>   
             
             
